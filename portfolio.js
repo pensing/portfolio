f20a29e43600a909148bb1d5d4ed1b6a49268fb3
paul <p.ensing66@gmail.com>
2018-12-13 11:29:56 +0100

    Portfolio van Paul Ensing

---
+++ portfolio.js
@@ -0,0 +1,16 @@
+// Declare a loop variable
+var i;
+
+// List View
+function listView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "100%";
+  }
+}
+
+// Grid View
+function gridView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "45%";
+  }
+}
